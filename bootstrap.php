2582d1ddbc7dcf52d96147e31b292dede0e20fab
Anthony Regeda <regeda@inbox.ru>
2015-04-01 18:48:38 +0300

    added cache for DI

--- bootstrap.php
+++ bootstrap.php
@@ -8,13 +8,33 @@ ini_set('session.use_cookies', 0);
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-$container = new ContainerBuilder();
+$cachedir = __DIR__ . '/cache';
+$cachefile = $cachedir .'/container.php';
 
-$loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
-$loader->load('parameters.yml');
-$loader->load('services.yml');
+if (file_exists($cachefile)) {
+    require_once $cachefile;
+    $container = new ProjectServiceContainer();
+} else {
+    $container = new ContainerBuilder();
+
+    $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
+    $loader->load('parameters.yml');
+    $loader->load('services.yml');
+
+    if (!$container->getParameter('dev_mode')) {
+        $container->compile();
+
+        $dumper = new PhpDumper($container);
+
+        if (!file_exists($cachedir)) {
+            mkdir($cachedir, 0777, true);
+        }
+        file_put_contents($cachefile, $dumper->dump());
+    }
+}
 
 if ($container->getParameter('dev_mode')) {
     ini_set('display_errors', 1);
